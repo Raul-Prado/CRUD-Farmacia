63435e4509546f27d8f4165754b124f63059339a
Raul-Prado <rauloliveiraprado@gmail.com>
2024-04-15 15:29:32 -0300

    Terceira Etapa — Construção da Model, Repository, Controller de Produto

--- src/main/java/com/generation/farmacia/model/Produto.java
+++ src/main/java/com/generation/farmacia/model/Produto.java
@@ -1,26 +1,25 @@
 package com.generation.farmacia.model;
 
-import java.util.List;
+import java.math.BigDecimal;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 @Entity
-@Table(name = "tb_farmacia")
-public class Categoria {
-	
+@Table(name = "tb_produto")
+public class Produto {
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -32,49 +31,65 @@ public class Categoria {
 	@Size(max = 220, message = "Descrição deve conter no maximo 220 caracter")
 	private String descricao;
 	
-	/*chave estrageira*/
-	@JsonIgnoreProperties("categoria")
-    @OneToMany(fetch = FetchType.LAZY, mappedBy = "categoria", cascade = CascadeType.REMOVE)
-    private List<Produto> produto;
+	@NotNull(message = "O preço do produto é obrigatório.")
+    @Column(precision = 8, scale = 2)
+	private BigDecimal preco;
 	
-	public List<Produto> getProduto() {
-		return produto;
-	}
-
-
-	public void setProduto(List<Produto> produto) {
-		this.produto = produto;
-	}
-
+	@NotNull(message = "A quantidade do produto é obrigatória.")
+	private int quantidade;
+	
+	/*chave estrageira*/
+	@JoinColumn(name = "categoria_id")
+	@JsonIgnoreProperties("produto")
+    @ManyToOne
+	private Categoria categoria;
 
 	public Long getId() {
 		return id;
 	}
 
-
 	public void setId(Long id) {
 		this.id = id;
 	}
-
 
 	public String getNome() {
 		return nome;
 	}
 
-
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-
 
 	public String getDescricao() {
 		return descricao;
 	}
 
-
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
 
+	public BigDecimal getPreco() {
+		return preco;
+	}
 
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
 }
